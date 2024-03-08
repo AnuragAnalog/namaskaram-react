630e9a4338248842e11bc3da016f0f08a87376a6
AnuragAnalog <anurag.peddi1998@gmail.com>
2024-03-08 17:55:30 -0500

    added some more class info

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,13 @@
-import React from "react"
+import { useState } from "react"
 
 function User(props) {
     const {name, location, contact} = props;
+    const [count, setCount] = useState(13);
 
     return (
         <>
             <div className="user-card">
-                <h2> Name: {name} </h2>
+                <h2> Name: {name}-{count} </h2>
                 <h3> Location: {location} </h3>
                 <h4> Contact: {contact} </h4>
             </div>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -3,16 +3,20 @@ import React from "react"
 class UserClass extends React.Component {
     constructor(props) {
         super(props)
-
-        console.log("UserClass constructor")
+        this.state = {
+            count: 13
+        }
     }
     render() {
         return (
             <>
                 <div className="user-card">
-                    <h2> Name: {this.props.name} </h2>
+                    <h2> Name: {this.props.name}-{this.state.count} </h2>
                     <h3> Location: {this.props.location} </h3>
                     <h4> Contact: {this.props.contact} </h4>
+                    <button onClick={() => {
+                        this.setState({count: this.state.count + 1})
+                    }}> Count Increase </button>
                 </div>
             </>
         )
